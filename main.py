8da29aefde5623f74d208a999d2ef1591a698dae
Anmol Madaik <anmol.m@idsil.loc>
2025-04-03 16:45:19 +0530

    Threading test

---
+++ main.py
@@ -0,0 +1,41 @@
+import multiprocessing as mp
+import threading
+
+
+class Test:
+
+    def __init__(self):
+        self.value = 1
+        self.lock = threading.Lock()
+
+    def update_value(self,value):
+        self.value = value
+
+    def print_value(self):
+        return self.value
+
+
+    def test(self,id):
+        # self.lock.acquire()
+        print(f'Old value {id}: {self.print_value()}')
+        self.update_value(id)
+        print(f'New value {id}: {self.print_value()}')
+        # self.lock.release()
+
+
+if __name__ == '__main__':
+    test = Test()
+
+    threads = []
+    for _ in range(5):  # Limit the number of threads
+        t1 = threading.Thread(target=test.test, args=(50,))
+        t2 = threading.Thread(target=test.test, args=(100,))
+        threads.append(t1)
+        threads.append(t2)
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join() 
+
