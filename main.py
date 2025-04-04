36d61cd3bb4590894011d6806ae252c98288ad70
Anmol Madaik <anmol.m@idsil.loc>
2025-04-04 10:53:44 +0530

    Sharing another class attributes

--- main.py
+++ main.py
@@ -3,45 +3,61 @@ import os
 import threading
 
 
-class Test:
+class Test2:
 
-    def __init__(self):
-        self.value = mp.Value('i',1)
-        self.lock = mp.Lock()
-        self.queue = mp.Queue()
+    def __init__(self,manager,lock):
+        self.value = manager.Value('i',1)
+        self.lock = lock
+        self.data = manager.Value('i',5)
 
-    def update_value(self,value):
-        self.value.value = value
 
     def print_value(self):
         return self.value.value
-
-
-    def test(self,id):
+    
+    def update_value(self,value):
         self.lock.acquire()
-        print(f'Old value {os.getpid()}: {self.print_value()}')
-        value = self.queue.get() + 10
-        self.update_value(value)
-        print(f'New value {os.getpid()}: {self.print_value()}')
-        self.queue.put(self.print_value())
+        self.value.value = value
         self.lock.release()
 
 
+    def print_data(self):
+        return self.data.value
+    
+    def update_data(self,value):
+        self.data.value = value
+
+class Test:
+
+    def __init__(self,manager):
+        self.value = manager.Value('i',1)
+        self.lock = mp.Lock()
+        self.test2_instance = Test2(manager,self.lock)
+
+
+    def test(self,id):
+        print(f'Old value {os.getpid()}: {self.test2_instance.print_value()}')
+        value = self.test2_instance.print_value()
+        self.test2_instance.update_value(value + 10)
+        print(f'New value {os.getpid()}: {self.test2_instance.print_value()}')
+        print(f'Old Data {os.getpid()}: {self.test2_instance.print_data()}')
+        data = self.test2_instance.print_data()
+        self.test2_instance.update_data(data + 10)
+        print(f'New value {os.getpid()}: {self.test2_instance.print_data()}')
+
 if __name__ == '__main__':
-    test = Test()
+    with mp.Manager() as manager:
+        test = Test(manager)
+        threads = []
+        print(mp.cpu_count())
+        for _ in range(5):  # Limit the number of threads
+            t1 = mp.Process(target=test.test, args=(50,))
+            t2 = mp.Process(target=test.test, args=(100,))
+            threads.append(t1)
+            threads.append(t2)
 
-    threads = []
-    test.queue.put(10)
-    print(mp.cpu_count())
-    for _ in range(5):  # Limit the number of threads
-        t1 = mp.Process(target=test.test, args=(50,))
-        t2 = mp.Process(target=test.test, args=(100,))
-        threads.append(t1)
-        threads.append(t2)
+        for t in threads:
+            t.start()
 
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join() 
+        for t in threads:
+            t.join() 
 
